0954808c923ac856e8e6c769f3a1ace73b65d71b
Daniel <danielmasmar@gmail.com>
2024-04-10 18:03:28 -0400

    commit

--- src/utils/datos.js
+++ src/utils/datos.js
@@ -1,30 +1,79 @@
 module.exports = [
-    {
-      "ISBN": "9780345803474",
-      "book_title": "To Kill a Mockingbird",
-      "author": "Harper Lee",
-      "book_cover_url": "https://media.glamour.com/photos/56e1f3c4bebf143c52613c00/master/w_1280,c_limit/entertainment-2016-02-06-main.jpg",
-      "genre": "Fiction",
-      "book_description": "Scout Finch, daughter of the town lawyer Atticus, has just started school; but her carefree days come to an end when a black man in their community is accused of raping a white woman, and Scout's father is appointed to defend him.",
-      "price": 10.99
-    },
-    {
-      "ISBN": "9780547249643",
-      "book_title": "The Hunger Games",
-      "author": "Suzanne Collins",
-      "book_cover_url": "https://www.panempropaganda.com/picture/hunger-games-uk-origianl%20katniss%20book%20cover.jpg?pictureId=21964125&asThumbnail=true",
-      "genre": "Science Fiction",
-      "book_description": "In a dystopian future, Katniss Everdeen volunteers to take her sister's place in the Hunger Games, a televised competition where teenagers fight to the death in a post-apocalyptic world.",
-      "price": 12.99
-    },
-    {
-      "ISBN": "9780385511616",
-      "book_title": "Water for Elephants",
-      "author": "Sara Gruen",
-      "book_cover_url": "https://m.media-amazon.com/images/I/519ng1DJRPL._SY445_SX342_.jpg",
-      "genre": "Historical Fiction",
-      "book_description": "In the 1930s, Jacob Jankowski, a veterinary student, joins the Benzini Brothers Circus as their resident vet, where he falls in love with Marlena, the beautiful equestrian star, and befriends Rosie, the captivating elephant.",
-      "price": 12.99
-    }
-  ]
-  
+  {
+    ISBN: "9780345803474",
+    book_title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    book_cover_url:
+      "https://media.glamour.com/photos/56e1f3c4bebf143c52613c00/master/w_1280,c_limit/entertainment-2016-02-06-main.jpg",
+    genre: "Fiction",
+    book_description:
+      "Scout Finch, daughter of the town lawyer Atticus, has just started school; but her carefree days come to an end when a black man in their community is accused of raping a white woman, and Scout's father is appointed to defend him.",
+    price: 10.99,
+  },
+  {
+    ISBN: "9780547249643",
+    book_title: "The Hunger Games",
+    author: "Suzanne Collins",
+    book_cover_url:
+      "https://www.panempropaganda.com/picture/hunger-games-uk-origianl%20katniss%20book%20cover.jpg?pictureId=21964125&asThumbnail=true",
+    genre: "Science Fiction",
+    book_description:
+      "In a dystopian future, Katniss Everdeen volunteers to take her sister's place in the Hunger Games, a televised competition where teenagers fight to the death in a post-apocalyptic world.",
+    price: 12.99,
+  },
+  {
+    ISBN: "9780385511616",
+    book_title: "Water for Elephants",
+    author: "Sara Gruen",
+    book_cover_url:
+      "https://m.media-amazon.com/images/I/519ng1DJRPL._SY445_SX342_.jpg",
+    genre: "Historical Fiction",
+    book_description:
+      "In the 1930s, Jacob Jankowski, a veterinary student, joins the Benzini Brothers Circus as their resident vet, where he falls in love with Marlena, the beautiful equestrian star, and befriends Rosie, the captivating elephant.",
+    price: 12.99,
+  },
+  {
+    ISBN: "9789589016114",
+    book_title: "La Fiesta del Chivo",
+    author: "Mario Vargas Llosa",
+    book_cover_url:
+      "https://ellector.com.py/product_photos/24b89be1/3c971739655f834805aa13f77cfdd7a3.jpg",
+    genre: "Ficcion",
+    book_description:
+      "En La Fiesta del Chivo (2000) asistimos a un doble retorno. Mientras Urania visita a su padre en Santo Domingo, volvemos a 1961, cuando la capital dominicana aún se llamaba Ciudad Trujillo. Allí un hombre que no suda tiraniza a tres millones de personas sin saber que se gesta una maquiavélica transición a la democracia.",
+    price: 15.99,
+  },
+  {
+    ISBN: "784-025100508-8",
+    book_title: "Fuenteovejuna",
+    author: "Lope de Vega",
+    book_cover_url:
+      "https://ellector.com.py/product_photos/24b89be1/ce91189dfcd9eb824bc48b85e323436f.jpg",
+    genre: "Narrativa",
+    book_description:
+      "En Fuenteovejuna las sabias combinaciones rítmicas dan una ágil eficiencia a los planteamientos dramáticos; con la hechura de los personajes, sesgada pero nítida. y el clímax del conflicto hacen de esta pieza una de las cimas del arte escénico.",
+    price: 10.0,
+  },
+  {
+    ISBN: "978-846634243-8",
+    book_title: "Fuera de Serie Outliers",
+    author: "Malcolm Gladwell",
+    book_cover_url:
+      "https://ellector.com.py/product_photos/24b89be1/9f82c9b2de29fee8c2d252e66a0ae97e.jpg",
+    genre: "Motivacion",
+    book_description:
+      "A través de su viaje por el mundo de los «fuera de serie», los mejores, los más brillantes y famosos, nos convence de que nuestro modo de pensar en el éxito es erróneo.Prestamos demasiada atención al aspecto de estas personas, y muy poca al lugar de donde vienen, es decir, a su cultura, su familia, su generación y las singularidades de su educación. Brillante y entretenido, Fuera de serie es toda una referencia que al mismo tiempo iluminará y hará disfrutar.",
+    price: 10.0,
+  },
+  {
+    ISBN: "978-841839563-5",
+    book_title: "Robinson Crusoe Td",
+    author: "Defoe, Daniel",
+    book_cover_url:
+      "https://ellector.com.py/product_photos/24b89be1/3a42fcf346f04b514de4707bb1590a91.jpg",
+    genre: "Aventura",
+    book_description:
+      "Tras una travesía llena de aventuras y peligros, el marinero Robinson Crusoe naufraga en una isla desierta donde le esperan más de dos décadas de absoluta soledad.",
+    price: 30.0,
+  },
+];
