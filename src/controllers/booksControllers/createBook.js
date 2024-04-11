cf2bd997b5f05255901f5fbdf95c54a371eb8140
MichaelKMedina <maicollagos@gmail.com>
2024-04-11 13:21:32 -0300

    reestructuracion

---
+++ src/controllers/booksControllers/createBook.js
@@ -0,0 +1,24 @@
+const { book } = require("../../db");
+
+const createBook = async ({
+  ISBN,
+  book_title,
+  author,
+  book_cover_url,
+  genre,
+  book_description,
+  price,
+}) => {
+  const newbook = await book.create({
+    ISBN,
+    book_title,
+    author,
+    book_cover_url,
+    genre,
+    book_description,
+    price,
+  });
+  return newbook;
+};
+
+module.exports = createBook;
--- src/utils/montarBaseDeDatos.js
+++ src/utils/montarBaseDeDatos.js
@@ -1,9 +1,9 @@
 const datos = require("./datos");
-const createabook = require("../routes/bookhandler/controllers/createaBook");
+const createBook = require("../controllers/booksControllers/createBook");
 
 module.exports = () => {
   datos.forEach((book) => {
-    createabook(book);
+    createBook(book);
   });
   console.log("Datos de la API externa guardados en la base de datos.");
 };
