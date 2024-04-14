bb4faf987b489b3f250fd7dafafe06f3d18bfc8b
Michael Medina Lagos <122126112+MichaelKMedina@users.noreply.github.com>
2024-04-14 19:32:57 -0300

    Update createBookSchema.js

--- src/schemas/createBookSchema.js
+++ src/schemas/createBookSchema.js
@@ -3,15 +3,15 @@ const Joi = require("joi");
 const createBookSchema = Joi.object({
   ISBN: Joi.number().integer().min(1).max(9999999999999).required(),
 
-  book_title: Joi.string().max(255).required(),
+  book_title: Joi.string().min(0).max(255).required(),
 
-  author: Joi.string().max(100).required(),
+  author: Joi.string().min(1).max(100).required(),
 
   book_cover_url: Joi.any(),
 
-  genre: Joi.string().max(100).required(),
+  genre: Joi.string().min(1).max(100).required(),
 
-  book_description: Joi.string().max(2000).required(),
+  book_description: Joi.string().min(1).max(2000).required(),
 
   price: Joi.number().precision(2).min(0).max(999999).required(),
 
