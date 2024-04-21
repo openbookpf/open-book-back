6165ed24024c58ce50021de42d9a585d74d00d6a
Michael Medina Lagos <122126112+MichaelKMedina@users.noreply.github.com>
2024-04-21 19:09:05 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -17,17 +17,17 @@ const sequelize = new Sequelize(
   }
 );
 
-// const sequelize = new Sequelize(DB_DEPLOY, {
-//    dialect: "postgres",
-//    logging: false,
-//   native: false,
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//        rejectUnauthorized: false, // Usar false si no tienes un certificado de CA válido
-//      },
-//     },
-//   });
+ const sequelize = new Sequelize(DB_DEPLOY, {
+    dialect: "postgres",
+    logging: false,
+   native: false,
+   dialectOptions: {
+     ssl: {
+       require: true,
+        rejectUnauthorized: false, // Usar false si no tienes un certificado de CA válido
+      },
+     },
+   });
 
 userModel(sequelize);
 bookModel(sequelize);
