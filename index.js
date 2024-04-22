3b1aca04b016aae2cd1eab82d43c655c41289968
Michael Medina Lagos <122126112+MichaelKMedina@users.noreply.github.com>
2024-04-22 13:00:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { conn } = require("./src/db.js");
 const montarBaseDeDatos = require("./src/utils/montarBaseDeDatos");
 
 conn
-  .sync({ force: true })
+  .sync({ alter: true })
   .then(() => {
     server.listen(port, async () => {
       console.log(`Server listening on port ${port}`);
