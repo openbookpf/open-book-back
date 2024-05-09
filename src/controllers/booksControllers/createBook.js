2998aeae7a2854d8bc2fabe06601e5c97f82a8d1
GFernandeFleitas <derlis.fernandez15@gmail.com>
2024-05-09 14:24:59 -0400

    integrated new create book

--- src/controllers/booksControllers/createBook.js
+++ src/controllers/booksControllers/createBook.js
@@ -16,21 +16,21 @@ const createBook = async ({
 }) => {
   // Verificar si el autor existe o crear uno nuevo
   let newAuthor = await author.findOne({ where: { name: authorName } });
-  if(!newAuthor){
-    newAuthor = await author.create({name: authorName, description: ''})
-  };
+  if (!newAuthor) {
+    newAuthor = await author.create({ name: authorName, description: "" });
+  }
 
   // Verificar si los géneros existen o crear nuevos
   const newGenres = await Promise.all(
     genresNames.map(async (genreName) => {
-      let addGenre
+      let addGenre;
       const newGenre = await genre.findOne({
         where: { name: genreName },
       });
-      if (newGenre){
-        return newGenre.id
-      }else{
-       addGenre = await genre.create({name: genreName})
+      if (newGenre) {
+        return newGenre.id;
+      } else {
+        addGenre = await genre.create({ name: genreName });
       }
       return addGenre.id;
     })
@@ -68,3 +68,74 @@ const createBook = async ({
 };
 
 module.exports = createBook;
+
+// const { book, genre, author, editorial, language } = require("../../db");
+// const getBookByIdController = require("./getBookByIdController");
+
+// const createBook = async ({
+//   ISBN,
+//   book_title,
+//   author: authorName,
+//   genre: genresNames,
+//   book_description,
+//   price,
+//   book_cover_url,
+//   editorial: editorialName,
+//   year_of_edition,
+//   language: languageName,
+//   age_segment,
+// }) => {
+//   // Verificar si el autor existe o crear uno nuevo
+//   let newAuthor = await author.findOne({ where: { name: authorName } });
+//   if(!newAuthor){
+//     newAuthor = await author.create({name: authorName, description: ''})
+//   };
+
+//   // Verificar si los géneros existen o crear nuevos
+//   const newGenres = await Promise.all(
+//     genresNames.map(async (genreName) => {
+//       let addGenre
+//       const newGenre = await genre.findOne({
+//         where: { name: genreName },
+//       });
+//       if (newGenre){
+//         return newGenre.id
+//       }else{
+//        addGenre = await genre.create({name: genreName})
+//       }
+//       return addGenre.id;
+//     })
+//   );
+
+//   // Verificar si la editorial existe o crear una nueva
+//   let [newEditorial] = await editorial.findOrCreate({
+//     where: { name: editorialName },
+//   });
+
+//   // Verificar si el idioma existe o crear uno nuevo
+//   let [newLanguage] = await language.findOrCreate({
+//     where: { name: languageName },
+//   });
+
+//   // Crear el libro y establecer las relaciones con autor, género, editorial y idioma
+//   const newBook = await book.create({
+//     ISBN,
+//     book_title,
+//     book_description,
+//     price,
+//     book_cover_url,
+//     year_of_edition,
+//     age_segment,
+//     // Establecer las relaciones usando las claves externas
+//     authorId: newAuthor.id,
+//     editorialId: newEditorial.id,
+//     languageId: newLanguage.id,
+//   });
+
+//   // Establecer las relaciones
+//   await newBook.setGenres(newGenres);
+
+//   return "creado con exito";
+// };
+
+// module.exports = createBook;
